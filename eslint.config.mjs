fd96cc95f85d3a7684e30e676418eedc8a3dac2f
Siri <siri09.02.2006@gmail.com>
2026-05-21 22:55:21 +0530

    Fix NoteType and ESLint crash

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,12 +11,6 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals"),
-  {
-    rules: {
-      "@typescript-eslint/no-unused-vars": "warn",
-      "@typescript-eslint/no-explicit-any": "warn",
-    },
-  },
 ];
 
 export default eslintConfig;
